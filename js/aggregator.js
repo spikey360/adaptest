79d83d1fca5f35af6ea73ccb6c8d4794a33216ad
Compucrusade <spikey360@yahoo.co.in>
2014-01-16 14:33:10 +0530

    Fixed aggregator function, added minimum finder function

--- js/aggregator.js
+++ js/aggregator.js
@@ -1,4 +1,4 @@
-function fetchKhi(q_id,a,b){
+function fetchKhi(q_id,a,b,khi_x,khi_y){
 if(q_id.length==0 || q_id==0){
 	window.alert("Invalid question id");
 	return;
@@ -8,30 +8,55 @@ if(q_id.length==0 || q_id==0){
 		url: "../computation/getkhi/"+q_id,
 		data:{a:a,b:b}
 	}).done(function(x){
-		window.alert(x);
-		return x;
+		//return won't function properly since this is an async call
+		//so, update with this value when call finishes
+		khis[khi_x][khi_y]=parseFloat(x);
+		done++;
 	});
 }
 }
-
+var mp_a=0;
+var mp_b=0; //most probable, calculated after getting minimum χ
 var start_a=0.0;
 var start_b=0.0;
 var end_a=1.0;
 var end_b=10.0;
-
+var aq=0.0, bq=0.0; //quanta
+var khis=null;
+var c=0; var d=0; //array index counters
+var done=0; var total=0;
 function aggregateKhi(q_id,a_step,b_step){
-z=""
+khis=new Array(); aq=a_step; bs=b_step;
+total=((end_a-start_a)/a_step+1)*((end_b-start_b)/b_step+1); //+1 cause <= is given
 if(q_id.length==0 || q_id==0){
 	window.alert("Invalid question id");
 	return;
 }else{
-	for(;start_a<=end_a;start_a+=a_step){
-		for(;start_b<=end_b;start_b+=b_step){
-			z+=fetchKhi(q_id,start_a,start_b)+" ";
+	for(i=start_b;i<=end_b;i+=b_step){
+		khis[c]=new Array()
+		for(j=start_b;j<=end_a;j+=a_step){
+			fetchKhi(q_id,j,i,c,d++);
 		}
-		z+="\n";
+		d=0;c++;
 	}
-	document.getElementById('x').value=z;
+
 }
+}
+
+function getMinimumKhi(){
+if(done!=total){
+	window.alert("Only "+done+"/"+total+" task completed");
+	return;
+}
+min_yet=khis[0][0];
+for(i=0;i<khis.length;i++){
+	for(j=0;j<khis[i].length;j++){
+		if(khis[i][j]<min_yet){
+			min_yet=khis[i][j];
+			mp_a=i*as; mp_b=j*bs;
+		}
+	}
+}
+return min_yet;
 }
 
