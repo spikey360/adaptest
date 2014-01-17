5792678ee98d51eacd7b7565a7f5ab582edaeb18
Compucrusade <spikey360@yahoo.co.in>
2014-01-17 20:25:47 +0530

    Optimisation done as suggested by arthasdk, only one db call instead of n^2

--- handlers/computation.py
+++ handlers/computation.py
@@ -8,6 +8,11 @@ from models.dbhelper import InvalidIdError
 from handlers.performestimation import DistributionAnalyzer
 from google.appengine.ext import ndb
 
+answers=None
+correct_distribution=None
+total_distribution=None
+question=None
+
 start_a=0.0
 end_a=1.0
 start_b=0.0
@@ -27,28 +32,28 @@ def calculateKhiTerm(totalAttemptees,theta,observedP,a,b,c):
 	khi=totalAttemptees*math.pow((observedP-P),2)/(P*Q)
 	return khi
 
-def calculateKhiSquare(question,a,b,c):
-	(answers,correct_distribution,total_distribution)=DistributionAnalyzer().analyzeQuestion(question)
+def calculateKhiSquare(a,b,c):
+	
 	khiSquare=0.0
 	for (x,y) in correct_distribution.items():
 		#x=theta, y=p(theta)
 		khiSquare=khiSquare+calculateKhiTerm(total_distribution[x],x,y,a,b,c)
 	return khiSquare
 
-def calculateParameters(question):
+def calculateParameters():
 	global start_a, end_a, start_b, end_b, interval_a, interval_b
 	min_a=start_a
 	min_b=start_b
 	a=start_a
 	b=start_b
 	c=0.25
-	min_yet=math.sqrt(calculateKhiSquare(question,a,b,c))
+	min_yet=math.sqrt(calculateKhiSquare(a,b,c))
 	steps_a=(end_a-start_a)/interval_a
 	steps_b=(end_b-start_b)/interval_b
 	for i in range(int(steps_a)):
 		b=0.0
 		for j in range(int(steps_b)):
-			khi=math.sqrt(calculateKhiSquare(question,a,b,c))
+			khi=math.sqrt(calculateKhiSquare(a,b,c))
 			if khi<min_yet:
 				min_yet=khi
 				min_a=a
@@ -59,13 +64,14 @@ def calculateParameters(question):
 
 class GetKhi(webapp2.RequestHandler):
 	def get(self,q_id):
+		global answers,correct_distribution,total_distribution
 		q_key=int(q_id)
 		c=0.25 #will have to be made flexible
 		calc_a=calc_b=calc_c=0.0
-		question=None
 		try:
 			question=dbhelper.fetchQuestion(q_key)
-			(calc_a,calc_b,calc_c)=calculateParameters(question)
+			(answers,correct_distribution,total_distribution)=DistributionAnalyzer().analyzeQuestion(question)
+			(calc_a,calc_b,calc_c)=calculateParameters()
 		except InvalidIdError:
 			self.response.out.write("F") #a 404 page should not be given here
 		self.response.out.write("%f,%f,%f"%(calc_a,calc_b,calc_c))
