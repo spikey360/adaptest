6ca3fa3e39a4a59f1c601cb871010d57b844726a
Compucrusade <spikey360@yahoo.co.in>
2014-01-17 12:35:42 +0530

    calculateKhiSquare(4) now accepts Question

--- handlers/computation.py
+++ handlers/computation.py
@@ -19,12 +19,7 @@ def calculateKhiTerm(totalAttemptees,theta,observedP,a,b,c):
 	khi=totalAttemptees*math.pow((observedP-P),2)/(P*Q)
 	return khi
 
-def calculateKhiSquare(q_key,a,b,c):
-	question=None
-	try:
-		question=dbhelper.fetchQuestion(q_key)
-	except InvalidIdError:
-		raise
+def calculateKhiSquare(question,a,b,c):
 	(answers,correct_distribution,total_distribution)=DistributionAnalyzer().analyzeQuestion(question)
 	khiSquare=0.0
 	for (x,y) in correct_distribution.items():
@@ -39,8 +34,10 @@ class GetKhi(webapp2.RequestHandler):
 		b=float(self.request.get('b'))
 		c=0.25 #will have to be made flexible
 		ks=0.0
+		question=None
 		try:
-			ks=calculateKhiSquare(int(q_id),a,b,c)
+			question=dbhelper.fetchQuestion(q_key)
+			ks=calculateKhiSquare(question,a,b,c)
 		except InvalidIdError:
 			self.response.out.write("F") #a 404 page should not be given here
 		self.response.out.write("%f"%ks)
