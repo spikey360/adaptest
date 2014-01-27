311609687b71a6cbbb695445824d09d114896c95
arthasdk <arthasdk1@gmail.com>
2014-01-27 15:50:39 +0530

    minor changes
    changelog:
    1. Changed the params of the naive algorithm.
    2. Introduced globals for weightage of correct/incorrect/passs.
    3. Fixed(hopefully) decrease of b(theta) for failing question.
    
    feedback+testing req

--- handlers/globals.py
+++ handlers/globals.py
@@ -1,4 +1,7 @@
 # a simple global file which can be used if/when required by importing :) 
 # all the previous variables in this file has been deprecated!
 
-firstQuestion='5838406743490560'
+firstQuestion='4749890231992320'
+correctAnswer=1.0
+incorrectAnswer=-1.0
+passAnswer=0.0
