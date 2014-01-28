dbc0c874b736f47480ed50fbba0838ecc1b23074
Compucrusade <spikey360@yahoo.co.in>
2014-01-28 21:30:55 +0530

    AnsweredQuestion unit testing

--- test/model_objects.py
+++ test/model_objects.py
@@ -84,5 +84,40 @@ class QuestionAnswerTest(unittest.TestCase):
 		gi.put()
 		self.assertEquals(gi,fetchGlobal(user))
 		self.assertIsNone(fetchGlobal(None))
-		
+	
+	def test_answeredQuestion(self):
+		question=Question()
+		question.question="Hello World"
+		question.a=1.0
+		question.b=5.0
+		question.c=0.25
+		poster=users.get_current_user()
+		q_key=question.put()
+		a1=Answer()
+		a2=Answer()
+		a3=Answer()
+		a4=Answer()
+		a1.question=a2.question=a3.question=a4.question=q_key
+		a1.answer="a1"
+		a2.answer="a2"
+		a3.answer="a3"
+		a4.answer="a4"
+		a4.correct=True
+		a1.correct=a2.correct=a3.correct=False
+		a1.put()
+		a2.put()
+		a3.put()
+		a_key=a4.put()
+		########################
+		ret=insertQuestionAnswered(users.get_current_user(),q_key,a_key)
+		self.assertEquals('S',ret)
+		ret=insertQuestionAnswered(users.get_current_user(),q_key,a_key)
+		self.assertEquals('R',ret)
+		query=AnsweredQuestion.query().fetch()
+		self.assertEquals(1,len(query))
+		ret=update_or_Insert_QuestionTestModule(str(q_key.id()),str(a_key.id()),users.get_current_user(),1.0)
+		self.assertEquals('S',ret)
+		update_or_Insert_QuestionTestModule(str(q_key.id()),str(a_key.id()),users.get_current_user(),1.0)
+		query=AnsweredQuestion.query().fetch()
+		self.assertEquals(2,len(query))
 
